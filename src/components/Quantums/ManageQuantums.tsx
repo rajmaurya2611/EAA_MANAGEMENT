a5a4f1713c261444493fb189f32d2ea67bc643d7
Maurya <Raj.Maurya@motherson.com>
2025-04-03 01:35:40 +0530

    Manage fixed11

--- src/components/Quantums/ManageQuantums.tsx
+++ src/components/Quantums/ManageQuantums.tsx
@@ -299,3 +299,15 @@
 // };
 
 // export default ManageQuantums;
+
+
+function ManageNotes() {
+  return (
+    <div>
+      <h2>Manage Carousel</h2>
+      {/* Add your UI for managing existing carousels here */}
+    </div>
+  );
+}
+
+export default ManageNotes;
